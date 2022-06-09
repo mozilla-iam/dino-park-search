017cda17b9a5a98ac26df91d7d38df276bd3ccbd
Brett Kochendorfer <bkochendorfer@mozilla.com>
2022-06-09 10:08:07 -0500

    chore(build): Update build image to supported image. (#89)
    * chore(build): Update build image to supported image.
    
    * chore(build): Pull in username and password

--- terraform/codebuild/main.tf
+++ terraform/codebuild/main.tf
@@ -29,6 +29,19 @@ resource "aws_codebuild_project" "build" {
       name  = "DOCKER_REPO"
       value = aws_ecr_repository.registry.repository_url
     }
+
+    environment_variable {
+      name  = "DOCKERHUB_USERNAME"
+      type  = "SECRETS_MANAGER"
+      value = "/CodeBuild/dockerhub:username"
+    }
+
+    environment_variable {
+      name  = "DOCKERHUB_PASSWORD"
+      type  = "SECRETS_MANAGER"
+      value = "/CodeBuild/dockerhub:password"
+    }
+
   }
 
   source {
