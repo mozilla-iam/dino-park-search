a9752f6013bf091e1505dc2f0c41ed73aaef99e7
Bheesham Persaud <bhee@mozilla.com>
2026-08-07 15:47:49 -0400

    fixup(ci): remove webhook since it broke
    Jira: IAM-2055

--- terraform/codebuild/main.tf
+++ terraform/codebuild/main.tf
@@ -56,22 +56,6 @@ resource "aws_codebuild_project" "build" {
   }
 }
 
-# Unomment this section if you do want to build automatically on push
-resource "aws_codebuild_webhook" "webhook" {
-  project_name = aws_codebuild_project.build.name
-  filter_group {
-    filter {
-      type    = "EVENT"
-      pattern = "PUSH"
-    }
-
-    filter {
-      type    = "HEAD_REF"
-      pattern = "(^refs/heads/master$|^refs/tags/.*-(prod|test))"
-    }
-  }
-}
-
 #---
 # IAM configuration
 #---
